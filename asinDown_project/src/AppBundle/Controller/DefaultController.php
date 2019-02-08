04f9b2c6a15c80623a3a9fc32b7f20bccc08ae30
seruba <sergio_311096@hotmail.es>
2019-02-08 10:05:03 +0100

    subir barra de Navegación y proyecto de prueba de bootstrap

--- asinDown_project/src/AppBundle/Controller/DefaultController.php
+++ asinDown_project/src/AppBundle/Controller/DefaultController.php
@@ -9,12 +9,34 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/calendario", name="calendario")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('frontal/calendario.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/programas", name="programas")
+     */
+    public function programasAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/programas.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/perfil", name="perfil")
+     */
+    public function perfilAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/perfil.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
