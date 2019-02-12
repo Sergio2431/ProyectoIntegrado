a81bcc251fc14a09b04263645d4fcfd6cc3d907d
seruba <sergio_311096@hotmail.es>
2019-02-12 12:07:20 +0100

    subir FrontEnd para modificar

--- asinDown_project/src/AppBundle/Controller/DefaultController.php
+++ asinDown_project/src/AppBundle/Controller/DefaultController.php
@@ -1,89 +1,48 @@
 <?php
-
 namespace AppBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use AppBundle\Entity\Alumno;
-use AppBundle\Form\AlumnoType;
-
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/calendario", name="calendario")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('frontal/calendario.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
     /**
-    * @Route("/list_alumnos/", name="alumno")
-    */
-    public function alumno(Request $request)
+     * @Route("/programas", name="programas")
+     */
+    public function programasAction(Request $request)
     {
-      // replace this example code with whatever you need
-      return $this->render('Seguridad/list_alumnos.html.twig');
+        // replace this example code with whatever you need
+        return $this->render('frontal/programas.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
     }
-
     /**
-    *@Route("/list/", name="list")
-    */
-    public function listaAction(Request $request){
-      $repository = $this->getDoctrine()->getRepository(Alumno::class);
-      $Alumno = $repository->findAll();
-      return $this->render('Seguridad/list_alumnos.html.twig',array('Alumno' => $Alumno));
+     * @Route("/perfil", name="perfil")
+     */
+    public function perfilAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/perfil.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
     }
-
     /**
-    *@Route("/update/{username}", name="update")
-    */
-
-    public function ActuaAction(Request $request,$username)
-        {
-          if ($username) {
-            $repository = $this->getDoctrine()->getRepository(Alumno::class);
-              $Alumno = $repository->find($username);
-            }else {
-              $Alumno = new Alumno();
-            }
-            // creates a task and gives it some dummy data for this example
-
-            $form = $this->createForm(AlumnoType::class, $Alumno);
-            $form->handleRequest($request);
-
-                    if ($form->isSubmitted() && $form->isValid()) {
-                        $Alumno = $form->getData();
-
-                        $entityManager = $this->getDoctrine()->getManager();
-                        $entityManager->persist($Alumno);
-                        $entityManager->flush();
-
-                      }
-
-              return $this->render('Seguridad/nuevo.html.twig', array(
-                  'form' => $form->createView(),
-              ));
-          }
-          /**
-          * @Route("/delete/{username}", name="delete")
-          */
-          public function deleteAction($username)
-          {
-            $form = $this->getDoctrine()->getManager();
-
-            $Alumno = $form->getRepository('AppBundle:Alumno')->find($username);
-
-            if (!$Alumno) {
-        return $this->redirectToRoute('list');
-      }
-
-      $form->remove($Alumno);
-      $form->flush();
-
-      return $this->redirectToRoute('list');
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/login.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
     }
 }
