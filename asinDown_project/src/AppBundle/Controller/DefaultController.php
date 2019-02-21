4614c448719cafb7192c4eb1c9f4066e497c3b21
seruba <sergio_311096@hotmail.es>
2019-02-21 10:49:09 +0100

    Subir modificaciones en Front-End

--- asinDown_project/src/AppBundle/Controller/DefaultController.php
+++ asinDown_project/src/AppBundle/Controller/DefaultController.php
@@ -28,6 +28,28 @@ class DefaultController extends Controller
     }
 
     /**
+     * @Route("/personal", name="personal")
+     */
+    public function personalAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/personal.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/evento", name="evento")
+     */
+    public function eventoAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/evento.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
      * @Route("/perfil", name="perfil")
      */
     public function perfilAction(Request $request)
@@ -59,4 +81,71 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/registerAlumnos", name="registerAlumnos")
+     */
+    public function registerAlumnosAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('seguridad/register_alumnos.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/registerProfesores", name="registerProfesores")
+     */
+    public function registerProfesoresAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('seguridad/register_profesores.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/registerVoluntarios", name="registerVoluntarios")
+     */
+    public function registerVoluntariosAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('seguridad/register_voluntarios.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/registerAulas", name="registerAulas")
+     */
+    public function registerAulasAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('seguridad/register_aulas.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/registerAsignaturas", name="registerAsignaturas")
+     */
+    public function registerAsignaturasAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('seguridad/register_asignaturas.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/registerProgramas", name="registerProgramas")
+     */
+    public function registerProgramasAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('seguridad/register_programas.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
 }
